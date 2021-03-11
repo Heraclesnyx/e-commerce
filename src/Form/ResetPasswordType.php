704e5c7b8cbe44d03ca7a6b5f00e15a9dbb6c82f
Thomas <anthony1.reboul@eptech.eu>
2021-03-11 14:43:06 +0100

    Mise en place des pages(Mon compte/changer de mot de passe);ainsi que leur controller

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('old_password', PasswordType::class, [
+                'mapped' => false,        //mapped permet de ne pas lié se champ avec mon Entity (sa évite de le migrate dans la bdd)
+                'label' => "Mon mot de passe actuel",
+                'attr' => [
+                    'placeholder' => "Saisissez votre mot de passe"
+                ]
+            ])
+            ->add('new_password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => "le mot de passe et la confirmation doivent être identique",
+                'mapped' => false,        //mapped permet de ne pas lié se champ avec mon Entity (sa évite de le migrate dans la bdd)
+                'label' => "Votre nouveau mot de passe",
+                'required' => true,
+                'first_options'=> [
+                    'label' => "Votre nouveau mot de passe",
+                    'attr' => [
+                        'placeholder' => "Veuillez saisir votre nouveau mot de passe"
+                    ]
+                ],
+                'second_options'=>[
+                    'label' => "Confirmez votre nouveau mot de passe",
+                    'attr' => [
+                        'placeholder' => "Veuillez confirmer votre nouveau mot de passe"
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => "Mettre à jour"
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
