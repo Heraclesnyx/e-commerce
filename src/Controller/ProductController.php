833444d4ed737cae0d241c9a80adfa8f2c6296a1
Thomas <anthony1.reboul@eptech.eu>
2021-07-20 14:35:23 +0200

    Mise en place de la barre de filtre par catégory ou par nom d'un produit pas un user

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use App\Classe\Search;
+use App\Form\SearchType;
 use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -22,14 +25,25 @@ class ProductController extends AbstractController
     /**
      * @Route("/product", name="products")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        //Afficher mes produits
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
-        //dd($products);
+        //Formulaire pour la recherche d'un produit ou en fonction des categories
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+
+        if($form->isSubmitted() && $form->isvalid()){
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }else{
+            //Afficher tous mes produits
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
