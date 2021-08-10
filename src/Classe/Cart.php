876248bb52e3abcb5f57cd52c0c479baac4ec5bf
Thomas <anthony1.reboul@eptech.eu>
2021-08-10 15:56:22 +0200

    Mise en place de la session du panier, mise en place de l'ajout et de la suppression de la session et l'ajout d'un produit, on voit le résultat à l'aide du debug pour le moment (dd), pas de vue pour le moment

---
+++ src/Classe/Cart.php
@@ -0,0 +1,44 @@
+<?php
+
+//Gestion pour le panier avec les quantité et les sessions lié au panier
+
+
+namespace App\Classe;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class Cart
+{
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    //Ajouter des produits à mon panier
+    public function add($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if(!empty($cart[$id]))
+        {
+            $cart[$id]++;
+        }else{
+            $cart[$id] = 1;
+        }
+
+        $this->session->set('cart', $cart);
+    }
+
+
+    public function get()
+    {
+        return $this->session->get('cart');
+    }
+
+    public function remove()
+    {
+        return $this->session->remove('cart');
+    }
+}
