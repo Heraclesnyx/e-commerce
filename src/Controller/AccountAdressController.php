8be9ba1e54fae1370581bb36f1a5671c26d38fda
Thomas <anthony1.reboul@eptech.eu>
2021-08-12 15:35:13 +0200

    Mise en place de la gestion des adresse d'un utilisateur, création,modificatio,suppression et liste des adresses d'un user

--- src/Controller/AccountAdressController.php
+++ src/Controller/AccountAdressController.php
@@ -2,18 +2,104 @@
 
 namespace App\Controller;
 
+use App\Entity\Adress;
+use App\Form\AddressType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AccountAdressController extends AbstractController
 {
+    private $entityManager;
+
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/account/address", name="account_address")
      */
     public function index(): Response
     {
-        dd($this->getUser());
+//        dd($this->getUser());
         return $this->render('account/address.html.twig');
+    }
+
+    /**
+     * @Route("/account/add_address", name="account_address_add")
+     */
+    public function add(Request $request): Response
+    {
+//        dd($this->getUser());
+        $address = new Adress();
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $address->setUser($this->getUser());
+
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+//            dd($address);
+        }
+
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/account/edit_address/{id}", name="account_address_edit")
+     */
+    public function edit(Request $request, $id): Response
+    {
+//        dd($this->getUser());
+        $address = $this->entityManager->getRepository(Adress::class)->findOneBy(['id' => $id]);
+
+
+        if(!$address || $address->getUser() != $this->getUser()){
+            return $this->redirectToRoute('account_address');
+        }
+
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('account_address');
+        }
+
+
+        return $this->render('account/address_form.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/account/delete_address/{id}", name="account_address_delete")
+     */
+    public function delete($id): Response
+    {
+        $address = $this->entityManager->getRepository(Adress::class)->findOneBy(['id' => $id]);
+
+
+        if($address && $address->getUser() == $this->getUser()){
+            $this->entityManager->remove($address);
+            $this->entityManager->flush();
+        }
+            return $this->redirectToRoute('account_address');
+
     }
 }
