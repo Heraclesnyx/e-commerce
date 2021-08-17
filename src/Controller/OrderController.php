41f0cece3be5cf3a11b1c50c43ec062ccef60e30
Thomas <anthony1.reboul@eptech.eu>
2021-08-17 19:52:09 +0200

    Mise en place du la partie commande, on peut voir le récapitulatif de notre commande avec adresse choisit, transporteur choisit, et récap des produits de notre panier. Le tout sur la même page

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
 use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+    private $entityManger;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManger = $entityManager;
+    }
+
+
     /**
      * @Route("/commande", name="order")
      */
-    public function index(): Response
+    public function index(Cart $cart, Request $request): Response
     {
         //Si l'user n'a pas d'adresse, je le renvoie à la page de création d'une adresse
         if(!$this->getUser()->getAdresses()->getValues())
@@ -23,8 +36,77 @@ class OrderController extends AbstractController
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
+
+
         return $this->render('order/index.html.twig',[
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'cart' => $cart->getFull()  //getFull permet de récupérer toutes les infos du panier, se trouve dans src/Classe/Cart
         ]);
+    }
+
+    /**
+     * @Route("/commande/recapitulatif", name="order_recap", methods={"POST"})
+     */
+    public function add(Cart $cart, Request $request): Response
+    {
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $date = new \DateTime();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+//            dd($delivery);
+            $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();
+            $delivery_content .= '<br/>'. $delivery->getPhone();
+
+            if($delivery->getCompany())
+            {
+                $delivery_content .= '<br/>'. $delivery->getCompany();
+            }
+
+            $delivery_content .= '<br/>'. $delivery->getAdress();
+            $delivery_content .= '<br/>'. $delivery->getPostal(). ' '. $delivery->getCity();
+            $delivery_content .= '<br/>'. $delivery->getCountry();
+//            dd($delivery_content);
+
+            //Enregistrer ma commande entity Order()
+            $order = new Order();
+            $order->setUser($this->getUser());
+
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+
+            $this->entityManger->persist($order);
+
+            //Enregistrer mes produits entity OrderDetails()
+            foreach ($cart->getFull() as $product)
+            {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+
+                $this->entityManger->persist($orderDetails);
+            }
+
+//            $this->entityManger->flush();//Ici on flush le tout càd Order et OrderDetails
+            return $this->render('order/add.html.twig',[
+                'cart' => $cart->getFull(),//getFull permet de récupérer toutes les infos du panier, se trouve dans src/Classe/Cart
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
+
+        }
+        return $this->redirectToRoute('cart'); //Renvoie à la page mon panier
     }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -6,6 +6,7 @@ use App\Entity\Adress;
 use App\Entity\Carrier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -18,7 +19,7 @@ class OrderType extends AbstractType
 
         $builder
             ->add('addresses', EntityType::class, [
-                'label' => 'Choisissez votre adresse de livraison',
+                'label' => false,
                 'required' => true,
                 'class' => Adress::class,
                 'choices' => $user->getAdresses(),
@@ -32,6 +33,12 @@ class OrderType extends AbstractType
                 'class' => Carrier::class,
                 'multiple' => false,
                 'expanded' => true
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider ma commande',
+                'attr'=> [
+                    'class' => 'btn btn-success btn-block'
+                ]
             ])
         ;
     }
